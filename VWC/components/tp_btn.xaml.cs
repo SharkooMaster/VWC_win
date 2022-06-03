320591bd789e8935c7620a56061e1141ef71811b
Ali Al Rashini <44705253+SharkooMaster@users.noreply.github.com>
2022-06-03 23:32:15 +0200

    Add project files.

---
+++ VWC/components/tp_btn.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace vwc.components
+{
+    public partial class tp_btn : UserControl
+    {
+        public static readonly DependencyProperty inner_content_property =
+        DependencyProperty.Register("inner_content", typeof(string), typeof(tp_btn), new PropertyMetadata(string.Empty));
+
+        public string inner_content
+        {
+            get { return (string)GetValue(inner_content_property); }
+            set { SetValue(inner_content_property, value); }
+        }
+
+        public tp_btn()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ VWC/components/tp_inp.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace vwc.components
+{
+    /// <summary>
+    /// Interaction logic for tp_btn.xaml
+    /// </summary>
+    public partial class tp_inp : UserControl
+    {
+        public static readonly DependencyProperty title_property =
+            DependencyProperty.Register("title", typeof(string), typeof(tp_inp), new PropertyMetadata(string.Empty));
+
+        public string title
+        {
+            get { return (string)GetValue(title_property); }
+            set { SetValue(title_property, value); }
+        }
+
+
+        public static readonly DependencyProperty placeholder_property =
+            DependencyProperty.Register("placeholder", typeof(string), typeof(tp_inp), new PropertyMetadata(string.Empty));
+
+        public string placeholder
+        {
+            get { return (string)GetValue(placeholder_property); }
+            set { SetValue(placeholder_property, value); }
+        }
+
+
+        public tp_inp()
+        {
+            InitializeComponent();
+        }
+    }
+}
